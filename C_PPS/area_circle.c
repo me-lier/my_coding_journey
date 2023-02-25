9abdd5668ac206ee4d8c6b6562513ab94efcc39b
me-lier <126309422+me-lier@users.noreply.github.com>
2023-02-25 11:38:04 +0530

    Create area_circle.c

---
+++ C_PPS/area_circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float area ,r;
+    float pi=3.142;
+    printf("Enter the radius of the cirlce:- ");
+    scanf("%f",&r);
+    area = pi*r*r;
+    printf("The area of the circle is :- %f",area);
+    
+}
