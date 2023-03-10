7cb03f36b153382e1580e6119c55646b4b8967c8
me-lier <126309422+me-lier@users.noreply.github.com>
2023-03-11 01:04:43 +0530

    Create teble_x.c

---
+++ C_PPS/teble_x.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("ENTER YOUR NUMBER TO GET MULTIPLICTAIN TABLE FROM 1 TO 10 :-");
+    scanf("%d",&a);
+    printf("The following is the table of :- %d",a);
+    for(int i=1; 11>i>0; i++){
+    printf("\n:--> %d x %d = %d",a,i, i*a);
+    }
+
+}
