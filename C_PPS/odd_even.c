99956d2d9a78b05eceb4d716586c6b7b37ee4f86
me-lier <126309422+me-lier@users.noreply.github.com>
2023-03-11 00:26:33 +0530

    Create odd_even.c

---
+++ C_PPS/odd_even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("ENTER YOUR  NUMBER YO GET WHETHER IT IS ODD OR EVEN:- ");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("IT IS AN EVEN NUMBER");
+    } else {
+        printf("IT IS AN ODD NUMBER");
+    }
+}
