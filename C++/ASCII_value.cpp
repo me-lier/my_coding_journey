d604a50e4a766aa914bc3a3b187406ab91551f98
me-lier <126309422+me-lier@users.noreply.github.com>
2023-06-06 20:39:13 +0530

    Create ASCII_value.cpp

---
+++ C++/ASCII_value.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter any character to get  the  ASCII value :- ";
+    cin>>ch;
+    cout<<"The ASCII value of :- "<< ch <<" is = "<<(int(ch));
+}
