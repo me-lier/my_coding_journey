6233b15a077ebec772a3537019c69d2f6a80b87f
me-lier <126309422+me-lier@users.noreply.github.com>
2023-06-06 20:38:39 +0530

    Create product.cpp

---
+++ C++/product.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter Your Number to get product of the both numbers:-  ";
+    cin>>a>>b;
+    cout<<"The product of "<<a<<"*"<<b<<"="<<(a*b);
+}
