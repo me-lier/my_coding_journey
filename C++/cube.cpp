fadf3358b94d13e059565d69937f94ed8c89cdee
me-lier <126309422+me-lier@users.noreply.github.com>
2023-06-06 20:40:10 +0530

    Create cube.cpp

---
+++ C++/cube.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a Number to get a Cube of it:- ";
+    cin>>a;
+    cout<<"The Cube of given Number is :- "<<a<<" = "<<(a*a*a);
+}
