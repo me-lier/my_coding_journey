73e97967d39e7121485265e97ac2be648c1c9241
me-lier <126309422+me-lier@users.noreply.github.com>
2023-06-06 20:40:51 +0530

    Create sizeof_datatypes.cpp

---
+++ C++/sizeof_datatypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"-:The following is the size of the DATA-TYPES:- "<<endl;
+    cout<<"The sizeof(char) : "<<(sizeof(char))<<endl;
+    cout<<"The sizeof(int) : "<<(sizeof(int))<<endl;
+    cout<<"The sizeof(bool) : "<<(sizeof(bool))<<endl;
+    cout<<"The sizeof(short) : "<<(sizeof(short))<<endl;
+    cout<<"The sizeof(double) : "<<(sizeof(double))<<endl;
+    cout<<"The sizeof(float) : "<<(sizeof(float))<<endl;
+    cout<<"The sizeof(long) : "<<(sizeof(long))<<endl;
+    cout<<"The sizeof(long long) : "<<(sizeof(long long))<<endl;
+    cout<<"The sizeof(long double) : "<<(sizeof(long double))<<endl;
+}
